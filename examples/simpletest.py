09ba5c1b65e7223b867866e396195c8f868e3a3f
scammo <samuel.brinkmann@googlemail.com>
2015-07-13 19:55:39 +0200

    python3 examples

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python
-# Copyright (c) 2014 Adafruit Industries
-# Author: Tony DiCola
-#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
@@ -26,26 +22,10 @@
 
 import Python_BMP.BMP085 as BMP085
 
-# Default constructor will pick a default I2C bus.
-#
-# For the Raspberry Pi this means you should hook up to the only exposed I2C bus
-# from the main GPIO header and the library will figure out the bus number based
-# on the Pi's revision.
-#
-# For the Beaglebone Black the library will assume bus 1 by default, which is
-# exposed with SCL = P9_19 and SDA = P9_20.
 sensor = BMP085.BMP085()
 
-# Optionally you can override the bus number:
-#sensor = BMP085.BMP085(busnum=2)
 
-# You can also optionally change the BMP085 mode to one of BMP085_ULTRALOWPOWER,
-# BMP085_STANDARD, BMP085_HIGHRES, or BMP085_ULTRAHIGHRES.  See the BMP085
-# datasheet for more details on the meanings of each mode (accuracy and power
-# consumption are primarily the differences).  The default mode is STANDARD.
-#sensor = BMP085.BMP085(mode=BMP085.BMP085_ULTRAHIGHRES)
-
-print 'Temp = {0:0.2f} *C'.format(sensor.read_temperature())
-print 'Pressure = {0:0.2f} Pa'.format(sensor.read_pressure())
-print 'Altitude = {0:0.2f} m'.format(sensor.read_altitude())
-print 'Sealevel Pressure = {0:0.2f} Pa'.format(sensor.read_sealevel_pressure())
+print('Temperatur:'+sensor.read_temperature())
+print('Luftdruck:'+sensor.read_temperature())
+print('Höhe:'+sensor.read_altitude())
+print('Meeresspiegeldruck:'+sensor.read_sealevel_pressure())
